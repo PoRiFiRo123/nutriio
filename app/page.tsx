e57ff7edf1b648de031a7bc7e0236412665ad385
PoRiFiRo〥#0001 <104781994+PoRiFiRo123@users.noreply.github.com>
2023-12-07 21:51:21 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import Image from "next/image";
 import Hero from "./components/Hero";
 import Newest from "./components/Newest";
-import Footer from "./components/Footer";
 
 export const dynamic = "force-dynamic";
 
@@ -10,7 +9,6 @@ export default function Home() {
     <div className="bg-white pb-6 sm:pb-8 lg:pb-12">
       <Hero />
       <Newest />
-      <Footer />
     </div>
   );
 }
