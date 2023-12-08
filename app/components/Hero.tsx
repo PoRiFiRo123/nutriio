90235f130fca20211b35b2f3596930a519e357a6
PoRiFiRo〥#0001 <104781994+PoRiFiRo123@users.noreply.github.com>
2023-12-08 21:23:08 +0530

    Update Hero.tsx

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -26,7 +26,7 @@ export default async function Hero() {
         </div>
 
         <div className="mb-12 flex w-full md:mb-16 lg:w-2/3">
-          <div className="relative left-12 top-12 z-10 -ml-12 overflow-hidden rounded-lg bg-gray-100 shadow-lg md:left-16 md:top-16 lg:ml-0">
+{/*           <div className="relative left-12 top-12 z-10 -ml-12 overflow-hidden rounded-lg bg-gray-100 shadow-lg md:left-16 md:top-16 lg:ml-0">
             <Image
               src="https://cdn.sanity.io/images/mhnho42r/production/99d58f88814c9af2c5021fd59dc53ae30200e4db-3804x2084.jpg"
               alt="Great Photo"
@@ -35,7 +35,7 @@ export default async function Hero() {
               width={500}
               height={500}
             />
-          </div>
+          </div> */}
 
           <div className="overflow-hidden rounded-lg bg-gray-100 shadow-lg">
             <Image
